e7767f5fac7c5a6c93a884d61f3b7c567f91f5d0
Timothy Chen <timothy.chen188@gmail.com>
2025-04-18 00:39:22 +0100

    WiFi Code Setup
    Connected to WiFi
    (Must add arduino_secrets.h)
    and add:
    char ssid[] = "";  // your network SSID (name)
    char pass[] = "";  // your network password (use for WPA, or use as key for WEP)

--- Arduino Code/smartWateringCode/smartWateringCode.ino
+++ Arduino Code/smartWateringCode/smartWateringCode.ino
@@ -1,3 +1,16 @@
+#include <WiFiS3.h>
+#include "arduino_secrets.h"
+#include <Firebase.h>
+
+/* Test Mode */
+Firebase fb(REFERENCE_URL);
+
+
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
+/* Locked Mode (With Authentication)*/
+// Firebase fb(REFERENCE_URL, AUTH_TOKEN);
+
 // Pin definitions
 const int moistureSensorPin = A0;
 const int trigPin = 8;            // Ultrasonic sensor TRIG (connected to D8)
@@ -11,7 +24,36 @@ long distance;
 
 void setup() {
   // Start serial communication
-  Serial.begin(9600); 
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial to be ready (for boards with native USB)
+  } 
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+   // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.println("You're connected to the network");
+  // Serial.println(ssid);
+  // Serial.println(pass);
+  IPAddress ip = WiFi.localIP();
+  Serial.println("IP Address: ");
+  Serial.println(ip);
+
+
   // Initialize the sensors
   pinMode(ledPin, OUTPUT);  // LED Pin acting as the water pump
   pinMode(trigPin, OUTPUT); // Ultrasonic sensor TRIG pin 
@@ -22,6 +64,8 @@ void setup() {
   
 }
 
+
+
 void loop() {
 
   // Flashing LED Test
@@ -29,24 +73,52 @@ void loop() {
   // delay(1000);                  // Wait for 1 second
   // digitalWrite(ledPin, LOW);   // Turn the LED OFF
   // delay(1000);                  // Wait for 1 second
+
+  // ----- Ultrasonic Sensor Logic (Water Level Measurement)
+
+  // Clear trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Send ultrasonic pulse
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Sensor needs t his timing to trigger a proper sound pulse
+  digitalWrite(trigPin, LOW);
+
+  // Measure the echo duration
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculate the distance in cm
+  // 0.034 = speed of sound in cm per microsecond at room temperature
+  // Divided by 2 because the duration measured is for the round-trip, so half would be the one-way distance
+  // distance = time * speed
+  distance = duration * 0.034 / 2;
+
+   //Serial.print("Water Level Distance: ");
+   //Serial.print(distance);
+   //Serial.println(" cm");
+
   
-  // -----
+  // ----- Soil moisture sensor logic
 
   // Read soil moisture sensor
   moistureLevel = analogRead(moistureSensorPin); // Read the sensor value
-  Serial.print("Soil Moisture Level: ");
-  Serial.println(moistureLevel); // Print the moisture level to the Serial Monitor
+   //Serial.print("Soil Moisture Level: ");
+   //Serial.println(moistureLevel); // Print the moisture level to the Serial Monitor
 
   if (moistureLevel < 400) {
     // Soil is dry
     digitalWrite(ledPin, HIGH); // Turn on LED (simulate water pump)
-    Serial.println("Soil is dry - Pump ON");
+     //Serial.println("Soil is dry - Pump ON");
 
   } else {
     // Soil is wet
     digitalWrite(ledPin, LOW); // Turn off LED
-    Serial.println("Soil is wet - Pump OFF");
+     //Serial.println("Soil is wet - Pump OFF");
   }
+
+   //Serial.println("------");
+   
 
   delay(2000); //Wait 2 seconds before reading again
 }
